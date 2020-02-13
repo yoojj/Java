efc8f77299803015ecb3fa1cbc45a1a826566108
yjj <yoojj.xyz@gmail.com>
2020-02-14 05:44:34 +0900

    NoClassDefFoundError

--- java-se/src/error/NoClassDefFoundErrorTest.java
+++ java-se/src/error/NoClassDefFoundErrorTest.java
@@ -2,11 +2,13 @@ package error;
 
 import org.junit.Test;
 
+import xxx.Temp;
+
 public class NoClassDefFoundErrorTest {
 
 	// NoClassDefFoundError
 	// : 컴파일시 해당 클래스가 존재하나 런타임시 해당 클래스를 찾을 수 없거나
-	//   해당 클래스가 있는 경우 정적 변수, 정적 블록 초기화시 예외가 발생하는 경우  
+	//   해당 클래스가 있는 경우 정적 변수, 정적 블록 초기화시 예외가 발생하는 경우 발생  
 	
 	// + ClassNotFoundException
 
@@ -18,7 +20,7 @@ public class NoClassDefFoundErrorTest {
 
 		try {
 			
-			// bin.error.Temp.class 제거 후 실행 
+			// Temp.class 제거 후 실행 
 			final Temp t = new Temp();
 			
 		} catch (NoClassDefFoundError e){
@@ -28,7 +30,3 @@ public class NoClassDefFoundErrorTest {
 	}
 	
 }
-
-
-
-class Temp {}
