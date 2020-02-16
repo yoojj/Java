6b418efccb1412adac662d5bf0bb10964823b5a7
yjj <yoojj.xyz@gmail.com>
2020-02-17 07:44:18 +0900

    NotSerializableVO

---
+++ java-se/src/xxx/NotSerializableVO.java
@@ -0,0 +1,22 @@
+package xxx;
+
+public class NotSerializableVO {
+
+	private int num;
+	
+	private String str;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	
+}
