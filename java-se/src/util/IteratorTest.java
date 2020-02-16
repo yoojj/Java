f7090319013364cce0369b838d1b92dc2eb6265a
yjj <yoojj.xyz@gmail.com>
2020-02-16 17:10:51 +0900

    Iterator

--- java-se/src/util/IteratorTest.java
+++ java-se/src/util/IteratorTest.java
@@ -13,9 +13,12 @@ public class IteratorTest {
 	// Iterator
 	// : 반복자, 순환자  
 	// : 컬렉션에 저장된 원소에 접근하기 위한 인터페이스  
+	// : fail-fast 방식으로 컬렉션 순회중 객체 변경이 감지되면 순회를 중단하고 예외 리턴  
 
 	// + Enumeration (jdk 1.0)
-
+	
+	// + ConcurrentModificationException
+	
 	// sub interface 
 	// - ListIterator
 
