7b4d2de472ab4f70acfa41398df856be7a3e38f9
yjj <yoojj.xyz@gmail.com>
2020-05-27 08:18:26 +0900

    ArrayIndexOutOfBoundsExceptionTest

--- java-se/src/exception/ArrayIndexOutOfBoundsExceptionTest.java
+++ java-se/src/exception/ArrayIndexOutOfBoundsExceptionTest.java
@@ -1,30 +1,31 @@
 package exception;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ArrayIndexOutOfBoundsExceptionTest {
 
 	// ArrayIndexOutOfBoundsException
-	// : 유효하지 않는 인덱스로 배열에 접근을 시도할 경우 발생 
+	// : 유효하지 않은 인덱스로 배열 참조시 발생
 	
 	// + IndexOutOfBoundsException
 	// + StringIndexOutOfBoundsException
 	
 	
 	
-	@Test
-	public void test() {
+	@Test 
+	public void arrayTest() {
 		
 		try {
 			
-			final int[] array = {1,2,3,4,5};
+			final int[] arr = {1,2,3,4,5};
 			
-			System.out.println(array[5]);
+			System.out.println(arr[5]);
 		
 		} catch(ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
 		
 	}
-	
+
 }
