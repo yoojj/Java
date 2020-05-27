73fbb632364282806f29d031c8cc2513ff3f94c8
yjj <yoojj.xyz@gmail.com>
2020-05-27 17:51:17 +0900

    TryCatch

--- java-basic/src/basic/exception/TryCatch.java
+++ java-basic/src/basic/exception/TryCatch.java
@@ -4,18 +4,27 @@ import org.junit.Test;
 
 public class TryCatch {
 
-	@Test
+	@Test 
 	public void test() {
 		
 		try {
 			
-			// 예외 발생 코드 영역
-			final int[] array = {1,2,3,4,5};
+			/* 예외 발생 가능성이 있는 영역 */
 			
+			final int[] array = {1,2,3,4,5};
+
 			System.out.println(array[10]);
 			
-		} catch(Exception e) {
+			System.out.println("예외 발생시 실행되지 않음");
 		
+		} catch(ArrayIndexOutOfBoundsException e) {
+			
+			/* 예외가 발생할 경우 처리 영역 */
+			
+			System.out.println(e.getMessage());
+			
+		} catch(Exception e) {
+			
 			// 예외 메시지 출력 
 			System.out.println(e.getMessage());
 			System.out.println(e.getLocalizedMessage());
@@ -24,7 +33,7 @@ public class TryCatch {
 			System.out.println(e.toString());
 
 			// 예외가 발생한 메소드와 오류 메시지 전체를 단계별로 출력 
-			//e.printStackTrace();
+			e.printStackTrace();
 			
 			for(StackTraceElement trace : e.getStackTrace()) {
 				
