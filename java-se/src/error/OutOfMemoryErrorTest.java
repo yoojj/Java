9a6d85c0e8e9f8a8d26cf527183ef095f2ea37c5
yjj <yoojj.xyz@gmail.com>
2020-05-27 09:12:49 +0900

    OutOfMemoryErrorTest

--- java-se/src/error/OutOfMemoryErrorTest.java
+++ java-se/src/error/OutOfMemoryErrorTest.java
@@ -1,27 +1,44 @@
 package error;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class OutOfMemoryErrorTest {
 
 	// OutOfMemoryError
-	// : 메모리 부족으로 발생하는 오류 
+	// : 메모리 부족시 발생하는 오류 
 
-	// - heap 
+	// - Heap 
 	// - PermGen
+	// - GC Overhead limit
 	// - VM limit
+	// - etc.
 	
-	// +  StackOverflowError
+	// Heap
+	// : 힙 영역이 충분하지 않은 경우   
+	// : finalize 메소드를 과도하게 사용해 GC가 지연되며 메모리가 부족해지는 경우 
+	//   (Object.finalize()을 오버라이드한 메소드)
+	
+	// PermGen
+	// : 과도하게 많은 클래스 로딩으로 인해 PermGen 영역이 충분하지 않은 경우 
+	// -XX:PermSize=
+	// -XX:MaxPermSize= 
 	
 	
 	
-	@Test
-	public void vmLimit() {
+	@Test @Ignore 
+	public void VMlimitTest() {
 
-		int[] array = new int[Integer.MAX_VALUE];
-		
-		for(int i = 0; i < array.length; ++i)
-			array[i] = i + 1;
+		try {
+
+			final int[] array = new int[Integer.MAX_VALUE];
+			
+			for(int i = 0; i < array.length; ++i)
+				array[i] = i + 1;
+			
+		} catch (OutOfMemoryError e){
+			e.printStackTrace();
+        }
 		
 	}
 	
