e58569665c43949c7af2283bbf37abe0f21f5bd0
yjj <yoojj.xyz@gmail.com>
2020-05-27 18:57:52 +0900

    ArithmeticExceptionTest

--- java-se/src/exception/ArithmeticExceptionTest.java
+++ java-se/src/exception/ArithmeticExceptionTest.java
@@ -6,6 +6,7 @@ public class ArithmeticExceptionTest {
 
 	// ArithmeticException
 	// : 수학적으로 올바르지 못한 계산 시도시 발생 
+	// : 모든 산술 연산에 해당하지 않음
 	
 	
 	
@@ -14,7 +15,9 @@ public class ArithmeticExceptionTest {
 		
 		try {
 			
-			System.out.println( 10 % 0 );
+			// 정수 0으로 나눌 경우 예외가 발생하지만 
+			// 실수 0.0으로 나눌 경우 NaN 반환 
+			System.out.println( 10 % 0.0 );
 			
 		} catch(ArithmeticException e) {
 			e.printStackTrace();
