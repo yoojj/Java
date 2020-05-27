a8ece84f38189f366fa8c2b2dfa9140e997760fe
yjj <yoojj.xyz@gmail.com>
2020-05-27 09:30:37 +0900

    StackOverflowErrorTest

--- java-se/src/error/StackOverflowErrorTest.java
+++ java-se/src/error/StackOverflowErrorTest.java
@@ -5,20 +5,30 @@ import org.junit.Test;
 public class StackOverflowErrorTest {
 
 	// StackOverflowError
-	// : 스택 메모리 부족으로 발생하는 오류   
-	// : 일반적으로 재귀함수가 종료되지 않은 경우 발생   
+	// : 스택 메모리 부족시 발생하는 오류   
 	
-	// + OutOfMemoryError
+	// : 재귀 호출로 인한 오류시 종료 시점을 확인하고 
+	//   특별한 경우가 아니면 재귀 호출을 반복문으로 수정
+
+	// : 수많은 메소드와 로컬 변수 사용으로 인한 스택 메모리 부족시
+	//   JVM 옵션을 통해 스택 메모리 크기를 늘릴 것
 	
 	
 
 	@Test
 	public void test() {
 		
-		count(1);
+		try {
+
+			this.count(1);
+			
+		} catch (StackOverflowError e){
+			e.printStackTrace();
+        }
+		
 	}
 	
-	public void count(int i) {
+	private void count(int i) {
 	
 		System.out.println(i);
 		
