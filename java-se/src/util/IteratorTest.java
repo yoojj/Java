9a04cf81978758889ef11617ffb6ebbbe2d63293
yjj <yoojj.xyz@gmail.com>
2020-05-30 04:18:16 +0900

    IteratorTest

--- java-se/src/util/IteratorTest.java
+++ java-se/src/util/IteratorTest.java
@@ -11,18 +11,18 @@ import org.junit.Test;
 public class IteratorTest {
 
 	// Iterator
-	// : 반복자, 순환자  
-	// : 컬렉션에 저장된 원소에 접근하기 위한 인터페이스  
-	// : fail-fast 방식으로 컬렉션 순회중 객체 변경이 감지되면 순회를 중단하고 예외 리턴  
+	// : 반복자, 열거자, 순환자  
+	// : 컬렉션 객체에 저장된 요소에 접근하기 위한 인터페이스  
+	// : 요소 읽기와 삭제 기능 지원  
+	// : fail-fast 방식으로 객체 순회중 객체 변경이 감지되면 순회를 중단하고 예외 리턴  
+	// : 단방향 순회만 지원하며 양방향 순회가 필요한 경우 ListIterator 사용   
 
-	// + Enumeration (jdk 1.0)
 	
-	// + ConcurrentModificationException
+	// + Enumeration  (jdk 1.0)
+	// + ListIterator (jdk 1.2)
+	// + Spliterator  (jdk 1.8)
+
 	
-	// sub interface 
-	// - ListIterator
-
-
 	
 	@SuppressWarnings("null")
 	@Test @Ignore
@@ -30,17 +30,16 @@ public class IteratorTest {
 		
 		final Iterator<?> i = null;
 		
-		// 다음 원소가 있으면 true, 없으면 false 반환
+		// 다음 요소가 있으면 true 없으면 false 반환
 		i.hasNext();
 		
-		// 다음 원소 반환
+		// 다음 요소 반환
 		i.next();
 		
-		// next()로 반환된 원소 삭제 
+		// next()로 반환된 요소 삭제 
 		i.remove();
 		
-		// jdk8
-		// 조건 전까지 원소에 대해 주어진 작업을 반복
+		// 조건 전까지 요소에 대해 주어진 작업 반복 (jdk8)
 		i.forEachRemaining(null);
 
 	}
@@ -52,9 +51,8 @@ public class IteratorTest {
 		
 		final List<Integer> list = new ArrayList<>();
 		
-		while(i.hasNext()) {
+		while(i.hasNext()) 
 			list.add(i.next());
-		}
 		
 		list.forEach(System.out::println);
 		
