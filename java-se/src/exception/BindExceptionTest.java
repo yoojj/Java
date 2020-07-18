869fa3f9f7708e99a303d87d1ad875b99e21da74
yjj <yoojj.xyz@gmail.com>
2020-07-18 23:16:00 +0900

    BindExceptionTest

---
+++ java-se/src/exception/BindExceptionTest.java
@@ -0,0 +1,16 @@
+package exception;
+
+import org.junit.Test;
+
+public class BindExceptionTest {
+
+	// BindException
+	// : IP 주소나 포트가 이미 사용중인 경우 소켓을 바인드하지 못해 발생
+	
+	
+	@Test
+	public void test() {
+		
+	}
+	
+}
