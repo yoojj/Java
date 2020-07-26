0aee854a6ff58a5cd71c852ffc286a10a0eeb6ed
yjj <yoojj.xyz@gmail.com>
2020-07-26 17:57:07 +0900

    FileTest

--- java-se/src/io/FileTest.java
+++ java-se/src/io/FileTest.java
@@ -9,74 +9,83 @@ import org.junit.Test;
 public class FileTest {
 
 	// File
-	// : 실제가 파일이나 디렉토리가 아니고 
-	// : 파일이나 디렉토리의 정보를 제공하여 생성, 변경, 제거 
-	// : OS 파일 시스템에 의존하므로 주의 (파일 권한, 파일 위치 표기법 등)
+	// : 파일이나 디렉토리의 정보를 제공하여 생성, 변경, 제거 (실제 파일이나 디렉토리가 아님)
+	// : OS의 파일 시스템에 의존 (파일 권한, 파일 위치 표기법 등 주의)
 	
 	
+	
+	final String path = "/home/jj/바탕화면/test/";
+	final String file = "file.txt";
 	
 	@Test @Ignore
-	public void test() {
-	
-		final String path = "/home/jj/바탕화면/test/";
-		
-		final String file = "file.txt";
+	public void test() throws IOException {
 		
 		final File f = new File(path, file);
 		
-		// 해당하는 경로나 파일이 존재하는지 여부 반환 
+		// 경로나 파일 존재 여부 반환 
 		f.exists();
 		
-		// 경로가 존재하는지 여부 반환
+		// 경로 존재 여부 반환
 		f.isDirectory();
 		
-		// 경로 반환
+		// 절대 경로 반환
+		f.getAbsolutePath();
+		
+		// 정규 경로 반환 
+		f.getCanonicalPath();
+	
+		// 부모 경로 반환
 		f.getParent();
+		
+		// 부모 파일 반환  
 		f.getParentFile();
-		try {
-			f.getCanonicalPath();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		
 		// 경로가 존재한다면 크기 반환
 		f.getTotalSpace();
 		f.getFreeSpace();
 		f.getUsableSpace();
+
+		// 경로 내에 존재하는 파일 반환
+		f.list();
 		
-		// 파일이 존재하는지 여부 반환
+		// 파일 존재 여부 반환
 		f.isFile();
+
+		// 경로를 포함한 파일명 반환
+		f.getPath();
+		f.getAbsolutePath();
 		
 		// 숨은 파일인지 여부 반환
 		f.isHidden();
 		
-		// 확장자 포함한 파일명 반환
+		// 확장자를 포함한 파일명 반환
 		f.getName();
 		
 		// 파일명 수정 
 		f.renameTo(new File(path, "renameFile.txt"));
+		
+		// 파일이 마지막으로 수정된 시간 반환 
+		f.lastModified();
+		
+		// 파일이 마지막으로 수정된 시간 변경
+		f.setLastModified(0);
 		
 		// 해당하는 경로가 없다면 생성
 		f.mkdir();
 		f.mkdirs();
 		
 		// 해당하는 파일이 없다면 생성하고 참 반환
-		try {
-			f.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		f.createNewFile();
 		
-		// 경로와 파일명 함께 반환
-		f.getPath();
-		f.getAbsolutePath();
-		
-		// 해당하는 경로나 파일 삭제
+		// 해당 경로나 파일 삭제
 		f.delete();
 		
 		// jvm 종료시 해당 경로나 파일 삭제 
-		// 임시 파일이 필요한 경우 사용 
+		// 임시 파일을 사용한 경우 이를 제거하기 위해 사용 
 		f.deleteOnExit();
+		
+		// 임시 파일 생성 
+		File.createTempFile("", "");
 		
 	}
 
@@ -85,16 +94,12 @@ public class FileTest {
 		
 		// 유닉스, 리눅스 환경에서 권한 확인 및 설정
 		
-		final String path = "/home/jj/바탕화면/test/";
-		
-		final String file = "file.txt";
-		
 		final File f = new File(path, file);
 		
 		// 해당 파일을 읽을 수 있는지 여부 반환
 		f.canRead();
 		
-		// 해당 파일을 쓰기가 가능한지 여부 반환
+		// 해당 파일을 변경할 수 있는지 여부 반환
 		f.canWrite();
 		
 		// 해당 파일을 실행할 수 있는지 여부 반환
@@ -111,26 +116,29 @@ public class FileTest {
 		
 	}
 	
-	@Test
+	@Test 
 	public void fileListTest() {
 		
-		final String path = "/home/jj/바탕화면/test/";
-
 		final File f = new File(path);
-		
+
+		/*
 		final String[] list = f.list();
+
 		for(String file : list) {
-			System.out.println( file );
+			System.out.println(file);
 		}	
+		*/	
+
+		final File[] files = f.listFiles();
 		
-		final File[] listFiles = f.listFiles();
-		for(File file : listFiles) {
+		for(File file : files) {
 		
-			if( file.isFile() ) {
-				System.out.println( file.getName() );
+			if(file.isFile()) {
+				System.out.println(file.getName());
 			}
 			
 		}
+		
 	}
 	
 }
